8d0174d656b10b84aa6b096b3595e800978988e6
Visvk <viktor.sincak@gmail.com>
2015-06-24 18:29:25 +0200

    version to 0.0.2

--- FleetlogAPI.php
+++ FleetlogAPI.php
@@ -8,7 +8,7 @@
  * @package  Fleetlog-API
  * @author   Viktor Sincak <viktor@fleetlog.com.au>
  * @license  Apache-2.0
- * @version  0.0.1
+ * @version  0.0.2
  * @link     https://github.com/fleetlog/fleetlog-php
  */
 class FleetlogAPI
